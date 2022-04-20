29f96307bb9ee0f6aabae34c2f18cf1183e9e9da
lemiskutswa <kutswalemis16@gmail.com>
2022-04-20 17:43:13 +0300

    if else statements

---
+++ if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+int time = 22;
+
+if (time<10) {
+    printf("Good Morning");
+} else if (time < 20 ) {
+printf("Good Afternoon");
+} else {
+printf("Good Night1");
+}
+return (0);
+}
